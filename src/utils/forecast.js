f6202becad961ef246ee7059faada54d7ac5b97d
Robin Liu <robinliu@Robins-MacBook-Pro.local>
2020-03-03 21:36:07 -0500

    init

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/7f498c62648dbcd12565e645159a9b8a/" +
+    lat +
+    "," +
+    long;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to GeoCode services", undefined);
+    } else if (error) {
+      callback("Unable to find forecast. Try another search");
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ". It is currently " +
+          body.daily.data[0].temperatureMin +
+          " Degrees (F). There is a " +
+          body.daily.data[0].precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
